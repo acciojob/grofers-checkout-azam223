c445d96214591d41cbccdf51f8b730b5d95bdf2f
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 17:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+// ... (previous JavaScript code)
 
 const getSum = () => {
   // Select the table
@@ -18,15 +16,9 @@ const getSum = () => {
     totalPrice += price;
   });
 
-  // Create a new row and cell for the total
-  const newRow = table.insertRow(-1); // -1 to add the row at the end
-  const totalCell = newRow.insertCell(0);
-
-  // Set the text content of the total cell
-  totalCell.textContent = `Total Price: Rs ${totalPrice}`;
-
-  // Optional: You can add a class to style the total row
-  newRow.classList.add("total-row");
+  // Display the total price in the element with id "ans"
+  const ansElement = document.getElementById("ans");
+  ansElement.textContent = `Total Price: Rs ${totalPrice}`;
 };
 
 getSumBtn.addEventListener("click", getSum);
